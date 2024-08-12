5ab759cae21e21bf9d8716f8ddcaafea72e95ea5
Arturo Montiel Acuna <arturomntl97@gmail.com>
2024-08-12 14:15:41 -0400

    Vercel Analytics
    Added the vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="GeistMono.className">
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
   
